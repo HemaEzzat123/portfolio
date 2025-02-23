719934a90070075cd30679c93726e2f8d0ce4448
hemaezzat123 <hezzat964@gmail.com>
2025-02-23 15:07:14 +0200

    first push

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,10 +11,8 @@ const inter = Inter({
 
 export const metadata = {
   title: {
-    template:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS  ",
-    default:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS ",
+    template: "Ibrahim Ezzat ",
+    default: "Ibrahim Ezzat ",
   },
   description:
     "A unique creative portfolio with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout CodeBucks on youtube.",
