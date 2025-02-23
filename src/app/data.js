b4542dbc2022ceebdfbaf4e783bf659b8c940a35
hemaezzat123 <hezzat964@gmail.com>
2025-02-23 14:50:07 +0200

    first push

---
+++ src/app/data.js
@@ -0,0 +1,140 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Social-media",
+    description: " Social Media Platform ",
+    date: "2024-08-15",
+    demoLink: "https://github.com/HemaEzzat123/Social-Media",
+  },
+  {
+    id: 2,
+    name: "house-market-place",
+    description:
+      "A responsive and feature-rich web application built with Vite, React.",
+    date: "2024-012-20",
+    demoLink: "https://github.com/HemaEzzat123/house-market-place",
+  },
+  {
+    id: 3,
+    name: "GitHub-Finder",
+    description:
+      "A React application that allows users to search for GitHub profiles.",
+    date: "2024-10-15",
+    demoLink: "https://github.com/HemaEzzat123/GitHub-Finder",
+  },
+  {
+    id: 4,
+    name: "Dictionary-web-app",
+    description:
+      "Dictionary web application that allows users to search for words",
+    date: "2024-06-30",
+    demoLink: "https://dictionaryfrontendwebapp.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "E-commerce",
+    description: "Ecommerce application that allows users to buy products",
+    date: "2024-05-12",
+    demoLink: "https://github.com/HemaEzzat123/E-commerce-native",
+  },
+  {
+    id: 6,
+    name: "evograph",
+    description: "Full security and system bot application discord.",
+    date: "2025-02-10",
+    demoLink: "https://github.com/HemaEzzat123/evograph",
+  },
+  {
+    id: 7,
+    name: "Last Portfolio",
+    description: "Last Portfolio",
+    date: "2024-08-18",
+    demoLink: "https://ibrahim-ezzat.vercel.app/",
+  },
+  {
+    id: 8,
+    name: "Learning-English-Letters",
+    description: "Collaborative learning platform",
+    date: "2024-04-05",
+    demoLink: "https://hemaezzat123.github.io/Learning-English-Letters/",
+  },
+  {
+    id: 9,
+    name: "Queueing-Models-Simulator",
+    description:
+      "A JavaScript-based project to simulate and calculate queueing models.",
+    date: "2025-02-22",
+    demoLink: "https://queueing-models-calculations.vercel.app/",
+  },
+  {
+    id: 10,
+    name: "Bank System",
+    description: "Bank System Manager",
+    date: "2024-02-14",
+    demoLink: "https://github.com/HemaEzzat123/Bank-System",
+  },
+  {
+    id: 11,
+    name: "FeedBack-UI",
+    description: "FeedBack System",
+    date: "2024-04-14",
+    demoLink: "https://github.com/HemaEzzat123/FeedBack-UI",
+  },
+  {
+    id: 12,
+    name: "TwitterClone",
+    description: "Clone Twitter",
+    date: "2024-05-14",
+    demoLink: "https://hemaezzat123.github.io/TwitterClone/",
+  },
+  {
+    id: 13,
+    name: "Note-App",
+    description: "Note App",
+    date: "2024-03-14",
+    demoLink: "https://github.com/HemaEzzat123/Note-App",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/HemaEzzat123",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/ibrahim-ezzat-4b21a8234/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "../../resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
